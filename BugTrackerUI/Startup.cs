bba398ffa9937038b846ac4daa2f3f5a1bffa9f5
James Amattey <me@jamattey.com>
2022-06-05 01:03:08 +0000

    Created NavList

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -23,11 +23,12 @@ namespace BugTrackerUI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<IBugService, BugService>();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
